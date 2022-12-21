d5b40f25f97634b556a6839414964e3376d4124d
DayAguiar <dayanerocha1712@gmail.com>
2022-12-21 09:21:57 -0300

    atualizando

--- main.js
+++ main.js
@@ -1,5 +1,30 @@
-const { texto } = require(".index");
+const regexYoutube = /^(https?\:\/\/)?((www\.)?youtube\.com|youtu\.be)\/.+$/;
+const input = document.querySelector("#linktitulo");
+const buttonAdd = document.querySelector("#button-add");
+const modal = document.querySelector("dialog");
+const modalContainer = document.querySelector("#dialogcontainer");
 
-const regExp1 = /Play/gi;
+// input.addEventListener("input", (e) => {
+//   if (regexYoutube.test(input.value)) {
+//     console.log("link do youtube");
+//   }
+//   console.log("input", input.value);
+// });
 
-console.log(regExp1.test(texto));
+buttonAdd.value = "Buscar";
+
+input.addEventListener("input", (e) => {
+  if (regexYoutube.test(input.value)) {
+    buttonAdd.value = "Adicionar";
+    return;
+  }
+  buttonAdd.value = "Buscar";
+});
+
+buttonAdd.onclick = function () {
+  modal.showModal();
+};
+
+modal.addEventListener("click", () => modal.close());
+
+modalContainer.addEventListener("click", (event) => event.stopPropagation());
