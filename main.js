95fd17a9af40142719e881e3716bf758cd793eb6
Day Aguiar <dayanerocha1712@gmail.com>
2022-12-23 09:33:11 -0300

    inserindo api do youtube

--- main.js
+++ main.js
@@ -21,10 +21,48 @@ input.addEventListener("input", (e) => {
   buttonAdd.value = "Buscar";
 });
 
-buttonAdd.onclick = function () {
-  modal.showModal();
-};
-
 modal.addEventListener("click", () => modal.close());
 
 modalContainer.addEventListener("click", (event) => event.stopPropagation());
+
+//TODO:Alterar nome da função execute e criar lógica para adicionar os resultados no modal.
+
+function execute(busca) {
+  gapi.client.youtube.search
+    .list({
+      part: ["snippet"],
+      maxResults: 4,
+      q: busca,
+    })
+    .then(
+      function (response) {
+        //TODO: Inserir dados/itens do response.result dentro do modal.(Pode criar uma função)
+        console.log("Response", response.result);
+      },
+      function (err) {
+        console.error("Execute error", err);
+      }
+    );
+}
+
+buttonAdd.onclick = function () {
+  modal.showModal();
+  //TODO: Pegar valor do input e passar para função de busca.
+  execute("Pokemon");
+};
+
+gapi.load("client", () => {
+  gapi.client.init({
+    apiKey: "AIzaSyAXbYfGpos4PSbVs_pbeYvS-AR16nhhiRg",
+  });
+  gapi.client
+    .load("https://content.googleapis.com/discovery/v1/apis/youtube/v3/rest")
+    .then(
+      function () {
+        console.log("GAPI client loaded for API");
+      },
+      function (err) {
+        console.error("Error loading GAPI client for API", err);
+      }
+    );
+});
