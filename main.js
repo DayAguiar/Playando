290833c4733995e6cdbea4aea361eacfff65407d
Day Aguiar <dayanerocha1712@gmail.com>
2023-01-20 09:14:44 -0300

    atualizando

--- main.js
+++ main.js
@@ -3,16 +3,16 @@ const input = document.querySelector("#linktitulo");
 const buttonAdd = document.querySelector("#button-add");
 const modal = document.querySelector("dialog");
 const modalContainer = document.querySelector("#dialogcontainer");
+const cardContainer = document.querySelector("#cardContainer");
 
-// input.addEventListener("input", (e) => {
-//   if (regexYoutube.test(input.value)) {
-//     console.log("link do youtube");
-//   }
-//   console.log("input", input.value);
-// });
+function test(value) {
+  console.log(value);
+}
+
+var texto = input.value;
+console.log(texto);
 
 buttonAdd.value = "Buscar";
-
 input.addEventListener("input", (e) => {
   if (regexYoutube.test(input.value)) {
     buttonAdd.value = "Adicionar";
@@ -21,11 +21,19 @@ input.addEventListener("input", (e) => {
   buttonAdd.value = "Buscar";
 });
 
+buttonAdd.disabled = true;
+input.addEventListener("input", (e) => {
+  const conteudo = input.value;
+  if (conteudo !== null && conteudo !== "") {
+    buttonAdd.disabled = false;
+    return;
+  }
+  buttonAdd.disabled = true;
+});
+
 modal.addEventListener("click", () => modal.close());
 
 modalContainer.addEventListener("click", (event) => event.stopPropagation());
-
-//TODO:Alterar nome da função execute e criar lógica para adicionar os resultados no modal.
 
 function execute(busca) {
   gapi.client.youtube.search
@@ -36,8 +44,52 @@ function execute(busca) {
     })
     .then(
       function (response) {
-        //TODO: Inserir dados/itens do response.result dentro do modal.(Pode criar uma função)
-        console.log("Response", response.result);
+        const videos = response.result.items;
+        const formattedVideos = videos.map((video) => {
+          return {
+            id: video.id.videoId,
+            publishedAt: video.snippet.publishedAt,
+            title: video.snippet.title,
+            image: video.snippet.thumbnails.medium.url,
+            channelTitle: video.snippet.channelTitle,
+          };
+        });
+
+        const cardsHtml = formattedVideos
+          .map((formattedvideo) => {
+            return `
+          <div id="card">
+            <img src="${formattedvideo.image}" alt="" />
+            <div id="cardContent">
+              <div id="informationWrapper">
+                <a
+                  id="videoName"
+                  href="https://www.youtube.com/watch?v=${formattedvideo.id}"
+                  >${formattedvideo.title}</a
+                >
+                <span id="postedBy">${formattedvideo.channelTitle}</span>
+                <span id="date">${formattedvideo.publishedAt}</span>
+              </div>
+              <button class="buttonModal"
+               data-id="${formattedvideo.id}"
+                data-title="${formattedvideo.title}"
+                 data-channelTitle="${formattedvideo.channelTitle}"
+                 data-image="${formattedvideo.image}" 
+                 data-publishedAt="${formattedvideo.publishedAt}" 
+                 >Adicionar</button>
+            </div>
+          </div>
+          `;
+          })
+          .join("");
+
+        cardContainer.innerHTML = cardsHtml;
+        const cardsButtons = document.querySelectorAll(".buttonModal");
+        cardsButtons.forEach((cardButton) => {
+          cardButton.addEventListener("click", (event) => {
+            console.log(event.target.dataset);
+          });
+        });
       },
       function (err) {
         console.error("Execute error", err);
@@ -47,8 +99,8 @@ function execute(busca) {
 
 buttonAdd.onclick = function () {
   modal.showModal();
-  //TODO: Pegar valor do input e passar para função de busca.
-  execute("Pokemon");
+  const conteudo = input.value;
+  execute(conteudo);
 };
 
 gapi.load("client", () => {
